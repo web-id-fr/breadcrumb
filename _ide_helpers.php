67913ebfbfb62f3c043cc691eee4aafaba304818
JulienCauvin <33349012+JulienCauvin@users.noreply.github.com>
2022-11-18 14:46:30 +0100

    :sparkles: add breadcrumb method ide helper (#6)
    * :sparkles: add breadcrumb method ide helper
    
    * Fix styling
    
    Co-authored-by: JulienCauvin <JulienCauvin@users.noreply.github.com>

---
+++ _ide_helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Illuminate\Routing {
+    /**
+     * @see \WebId\Breadcrumb\Http\Middleware\RegisterBreadcrumb
+     *
+     * @method self breadcrumb(array $classMethod)
+     */
+    class Route
+    {
+        //
+    }
+}
