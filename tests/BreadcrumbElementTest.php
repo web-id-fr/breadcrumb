9831a318ea50a761c1a7c71a9517e7c3158818be
Charley Guenier <charley.guenier@gmail.com>
2024-04-07 10:14:09 +0200

    :safety_vest: Add some tests and validation

---
+++ tests/BreadcrumbElementTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace WebId\Breadcrumb\Tests;
+
+use Illuminate\Validation\ValidationException;
+use WebId\Breadcrumb\BreadcrumbElement;
+
+class BreadcrumbElementTest extends TestCase
+{
+    /**
+     * @dataProvider correctArrayDataProvider
+     */
+    public function test_it_can_make_itself_from_valid_array(array $array): void
+    {
+        $breadcrumbElement = BreadcrumbElement::make($array);
+        $this->assertInstanceOf(BreadcrumbElement::class, $breadcrumbElement);
+    }
+
+    /**
+     * @dataProvider incorrectArrayDataProvider
+     */
+    public function test_a_validation_exception_is_thrown_if_incorrect_array_is_provided_as_parameter(array $array): void
+    {
+        $this->expectException(ValidationException::class);
+        BreadcrumbElement::make($array);
+    }
+
+    public function test_it_can_convert_into_array(): void
+    {
+        $breadcrumbArray = [
+            'url' => 'https://www.url.com',
+            'title' => 'title',
+            'another_key' => 'something'
+        ];
+
+        $breadcrumbElement = BreadcrumbElement::make($breadcrumbArray);
+
+        $expected = ['url' => 'https://www.url.com', 'title' => 'title'];
+        $actual = $breadcrumbElement->toArray();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public static function incorrectArrayDataProvider(): array
+    {
+        return [
+            [[]],
+            [['foo' => 'bar', 'hello' => 'world']],
+            [['url' => 'https://www.url.com', 'foo' => 'bar']],
+            [['foo' => 'bar', 'title' => 'title']],
+            [['https://www.url.com', 'title']],
+            [['title' => 123, 'url' => 'not-an-url', 'another_key' => 'something']],
+        ];
+    }
+
+    public static function correctArrayDataProvider(): array
+    {
+        return [
+            [['url' => 'https://www.url.com', 'title' => 'title']],
+            [['title' => 'title', 'url' => 'https://www.url.com']],
+            [['title' => 'title', 'url' => 'https://www.url.com', 'another_key' => 'something']],
+            [['title' => '', 'url' => 'http://www.url.com', 'another_key' => 'something']],
+        ];
+    }
+}
